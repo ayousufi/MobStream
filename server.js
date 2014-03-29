008f719b1fa14a36259a4a19c12e61a1753fbc03
Neel Virdy <neelvirdy@gmail.com>
2014-03-29 04:41:20 -0400

    Added database creation, corrected home get/post, deleted extraneous file.

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ app.set('views', __dirname + '/templates'); // tell Express where to find templa
 app.use(express.bodyParser()); // definitely use this feature
 app.use(express.static(__dirname));
 
-app.get('*', function(request, response){
+conn.query('CREATE TABLE IF NOT EXISTS tracks (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, mobname TEXT, id TEXT);');
+
+app.post('/', function(request, response){
+	
+});
+
+app.get('/', function(request, response){
     response.render("index.html");
 });
 

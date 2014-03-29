ada6165b9d08ce3f6dedf459a23c4fe0f4f92ebd
hongyis <hongyis@mit.edu>
2014-03-29 14:43:15 -0400

    added new example html

---
+++ templates/rdio-simple/examples/rdio_consumer_credentials.js
@@ -0,0 +1,4 @@
+// You can get these by signing up for a developer account at:
+// http://developer.rdio.com/
+exports.RDIO_CONSUMER_KEY = "earb28gg9rys53asc67mu47h"
+exports.RDIO_CONSUMER_SECRET = "rrEb5QpFcu"
